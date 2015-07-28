04acf70b59d5a7d11ece4ec5211ac0837215c719
j-womack <womack.joshua@gmail.com>
2015-07-28 15:14:30 -0500

    Database

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50),
-    name VARCHAR(100),
-    release_date YEAR(4),
-    sales FLOAT(7,2),
-    genre VARCHAR(50),
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date YEAR(4) NOT NULL,
+    sales_in_millions FLOAT(15,2) NOT NULL,
+    genre VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
---
+++ albums_seeder.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+INSERT INTO albums(artist, name, release_date, genre, sales_in_millions)
+
+VALUES  ('Michael Jackson', 'Thriller', 1982, 'Pop, rock, R&B', 42.4),
+        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock', 22.7),
+        ('Whitney Houston / Various artists', 'The Bodyguard', 1992, 'Soundtrack/R&B, soul, pop', 27.4),
+        ('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock, progressive rock', 20.6),
+        ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 'Rock, soft rock, folk rock', 32.2),
+        ('AC/DC', 'Back in Black', 1980, 'Hard rock', 25.9),
+        ('Bee Gees / Various artists', 'Saturday Night Fever', 1977, 'Disco', 19),
+        ('Fleetwood Mac', 'Rumours', 1977, 'Soft rock', 27.9);
